fa0457007aa53e12a7d27ff16f4083411fcfade7
JonnyRobbie <marcodv@seznam.cz>
2017-01-31 23:27:45 +0100

    initial commit

---
+++ mangatraders.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import sys, os, shutil
+import urllib.request
+import zipfile
+
+server = "http://217.23.10.62/manga/"
+series = sys.argv[1]
+chapter = sys.argv[2]
+dirname = "/tmp/" + series + "-" + chapter.zfill(4)
+page = 0
+responseCode = 0
+def urlname():
+    return server + series + "/" + chapter.zfill(4) + "-" + str(page).zfill(3) + ".png"
+if not os.path.exists(dirname):
+    os.mkdir(dirname)
+
+while responseCode != 404:
+    try:
+        page = page + 1
+        print("Retrieving " + urlname())
+        response = urllib.request.urlopen(urlname())
+        print("HTTP Response: " + str(response.getcode()))
+        responseCode = response.getcode()
+        with open(dirname + "/" + chapter.zfill(4) + "-" + str(page).zfill(3) + ".png", "wb") as f:
+            f.write(response.read())
+    except urllib.error.HTTPError:
+        print("HTTP Error " + str(sys.exc_info))
+        responseCode = 404
+print("Successfully retrieved " + str(page) + " pages.")
+
+try:
+    print("Compressing")
+    def zipdir(path, ziph):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                ziph.write(os.path.join(root, file))
+
+    zf = zipfile.ZipFile(dirname + ".zip", "w", zipfile.ZIP_DEFLATED)
+    zipdir(dirname + "/", zf)
+    zf.close()
+    print("Done")
+except:
+    print("Error in compression")
+print("Removing temp directory")
+shutil.rmtree(dirname)
